35370983f4c2c4551ef7ff048120b8f99ca38eb0
vkaza <kaza.vimal@gmail.com>
2020-03-05 01:12:33 -0500

    Simple Striim to Kafka

--- src/main/java/com/demo/engine/Consumer.java
+++ src/main/java/com/demo/engine/Consumer.java
@@ -12,7 +12,7 @@ public class Consumer {
 
     private final Logger logger = LoggerFactory.getLogger(Producer.class);
 
-    @KafkaListener(topics = "users", groupId = "group_id")
+    @KafkaListener(topics = "SQLData", groupId = "group_id")
     public void consume(String message) throws IOException {
         logger.info(String.format("#### -> Consumed message -> %s", message));
     }
